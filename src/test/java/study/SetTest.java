4821cea251b3d51802adba0b82c789baa31dda35
pyojungseon <suehig@naver.com>
2022-04-18 18:38:49 +0900

    test : Set parameter test
    Set size test
    Set contain value test
    Set contain value boolean test by csv

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,48 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("HashSet size check test")
+    void sizeTest() {
+        assertThat(numbers.size())
+                .isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("HashSet contain values test")
+    void containTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("HashSet contain values boolean test by csv parameterizedTest")
+    void containBooleanTest(int input, boolean expected) {
+        Assertions.assertEquals(numbers.contains(input), expected);
+    }
 }
