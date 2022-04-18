9a333509ac2ab9c0cef6a4a48968ec534781bb75
pyojungseon <suehig@naver.com>
2022-04-18 15:18:39 +0900

    feat : Make Constants
    Make constants class for Integer, String values, and game Messages

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class Constants {
+
+    public static final int CONTINUE=1;
+    public static final int END=2;
+    public static final int IS_STRIKE=0;
+    public static final int IS_BALL=1;
+
+    public static final String STRIKE="스트라이크";
+    public static final String BALL="볼";
+    public static final String NOTHING="낫싱";
+
+    public static final String INPUT_NUMBER_MENT="숫자를 입력해주세요 : ";
+    public static final String GAME_OVER_MENT="게임 종료";
+    public static final String THREE_STRIKE_MENT="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String DO_YOU_WANT_REGAME_MENT="게임을 새로 시작하려면 "+CONTINUE+", 종료하려면 "+END+"를 입력하세요.";
+}
