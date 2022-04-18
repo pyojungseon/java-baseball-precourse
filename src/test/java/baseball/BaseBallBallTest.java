ce92169484221966323a26defff6e59ea8986750
pyojungseon <suehig@naver.com>
2022-04-18 22:06:58 +0900

    test : make test code for ball, judge class
    make test code
    ball number test
    judge strike, ball, three strike test

--- src/test/java/baseball/BaseBallBallTest.java
+++ src/test/java/baseball/BaseBallBallTest.java
@@ -1,2 +1,15 @@
-package baseball;public class BaseBallBallTest {
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseBallBallTest {
+
+    @Test
+    void getNumberTest() {
+        BaseBallBall ball = new BaseBallBall();
+        ball.makeRandomNumber();
+        assertThat(ball.getNumber()).hasSize(3);
+    }
 }
--- src/test/java/baseball/BaseBallJudgeTest.java
+++ src/test/java/baseball/BaseBallJudgeTest.java
@@ -1,2 +1,30 @@
-package baseball;public class BaseBallJudgeTest {
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BaseBallJudgeTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:0", "3:0"}, delimiter = ':')
+    void strikeTest(char number, int strike) {
+        BaseBallJudge judge = new BaseBallJudge();
+        Assertions.assertEquals(judge.checkStrike(number, '1'), strike);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:1", "451:1", "789:0"}, delimiter = ':')
+    void ballTest(String number, int strike) {
+        BaseBallJudge judge = new BaseBallJudge();
+        Assertions.assertEquals(judge.checkBall(number, '1'), strike);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:true", "451:false", "862:false"}, delimiter = ':')
+    void threeStrikeTest(String number, boolean isStrike) {
+        BaseBallJudge judge = new BaseBallJudge();
+        judge.judgeNumber(number, "123");
+        Assertions.assertEquals(judge.isThreeStrike(), isStrike);
+    }
 }
